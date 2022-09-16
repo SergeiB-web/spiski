b0ccd22b5e628b90a6834c9d240311e22e9f81be
SergeiB-web <sbury7596@gmail.com>
2022-09-16 11:25:48 +0300

    Spiski

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-a = ["1", 235, True, None]
-for i in a:
-    print(i)
+a = [-5, 235, -2.32456, 0,34,-10,143,2367]
+for y in a:
+    b = a[1]
+    c = a[3]
+    d = a[6]
+sum = b+c+d
+print(sum)
+h = (a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7])/len(a)
+print(h)
